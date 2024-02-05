aa0989db291b4bf71315c1a63706e0dd0ced92a9
NyasimiPhilip <nyasimiphilip@gmail.com>
2024-02-05 19:20:51 +0300

    Update

--- app/src/main/java/com/android/unitconverter/compose/converter/TopScreen.kt
+++ app/src/main/java/com/android/unitconverter/compose/converter/TopScreen.kt
@@ -2,8 +2,10 @@ package com.android.unitconverter.compose.converter
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import com.android.unitconverter.data.Conversion
 import java.math.RoundingMode
 import java.text.DecimalFormat
@@ -15,6 +17,9 @@ fun TopScreen(list: List<Conversion>,
               typedValue : MutableState<String>,
               save: (String,String) -> Unit
 ) {
+    var toSave by remember {
+        mutableStateOf(false)
+    }
 
     ConversionMenu(list = list){
         selectedConversion.value = it
@@ -25,6 +30,7 @@ fun TopScreen(list: List<Conversion>,
         InputBlock(conversion = it, inputText = inputText) { input ->
             // Try to convert the input string to a double
             typedValue.value = input
+            toSave = true
         }
     }
     if(typedValue.value != "0.0"){
@@ -40,7 +46,10 @@ fun TopScreen(list: List<Conversion>,
         val message1 = "${typedValue.value} ${selectedConversion.value!!.convertFrom} is equal to"
         val message2 = "$roundedResult ${selectedConversion.value!!.convertTo}"
 
-        save(message1, message2)
+        if(toSave){
+            save(message1, message2)
+            toSave = false
+        }
         ResultBlock(message1 = message1, message2 = message2)
     }
 }
